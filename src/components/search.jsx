6a2508734394391490b0eb2c12f345e47d0e5886
santosh pal <santoshpal@santoshs-MacBook-Air.local>
2025-05-13 21:44:24 +0530

    updated

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -5,11 +5,11 @@ const SearchBar = () => {
   return (
     <div className="flex items-center gap-2 p-4 md:p-12 px-12 md:px-32 mt-6 mb-4">
       <div className="flex items-center px-2 py-2 md:px-4 md:py-4 rounded-lg md:rounded-xl border border-gray-300 w-full">
-        <Search className="text-[#2A5F72] mr-4" size={20} />
+        <Search className="text-[#2A5F72] mr-2 md:mr-4" size={20} />
         <input
           type="text"
           placeholder="Search tests..."
-          className="flex-grow outline-none text-gray-700 placeholder-gray-400"
+          className="flex-grow outline-none text-sm md:text-base text-gray-700 placeholder-gray-400"
         />
         <SlidersHorizontal className="text-[#2A5F72]" size={20} />
       </div>
@@ -21,3 +21,4 @@ const SearchBar = () => {
 };
 
 export default SearchBar;
+
